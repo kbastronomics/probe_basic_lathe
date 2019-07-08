e0f086cc3c5edfeb642c0f323370072f4a527a88
Sandra Carroll <smgvbest@gmail.com>
2019-07-07 21:20:11 -0400

    ORG: updated probe_basic.ui and setup.py

--- setup.py
+++ setup.py
@@ -4,23 +4,23 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="probe_basic",
+    name="probe_basic_lathe",
     version="0.0.1",
     author="Chris Polanski",
     author_email="",
-    description="Probe Basic - A QtPyVCP based Virtual Control Panel for LinuxCNC",
+    description="Probe Basic Lathe - A QtPyVCP based Virtual Control Panel for LinuxCNC",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/kcjengr/probe_basic",
-    download_url="https://github.com/kcjengr/probe_basic/tarball/master",
+    url="https://github.com/kbastronomics/probe_basic_lathe",
+    download_url="https://github.com/kbastronomics/probe_basic_lathe/tarball/master",
     packages=find_packages(),
     include_package_data=True,
     entry_points={
         'gui_scripts': [
-            'probe_basic=probe_basic:main',
+            'probe_basic_lathe=probe_basic_lathe:main',
         ],
         'qtpyvcp.vcp': [
-            'probe_basic=probe_basic',
+            'probe_basic_lathe=probe_basic_lathe',
         ],
     },
     install_requires=[
